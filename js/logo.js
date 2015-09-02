c810c4151326ecb103b4fc332ec4f85aafb712a5
Axel Smeets <murlocbrand+git@gmail.com>
2015-09-02 20:38:24 +0200

    Fix font weight/size/spacing + logo movement v2

--- js/logo.js
+++ js/logo.js
@@ -1,100 +1,79 @@
 window.onload = function() {
-
     var translate = function (el, x, y) {
         var style = 'translate(' + x + 'px, ' + y + 'px)'
+
         el.style.webkitTransform = style
         el.style.MozTransform = style
+        el.style.transform = style
     }
 
     var colors = [
-            [225, 0, 204],
-            [225, 0, 187],
-            [0, 212, 225],
-            [0, 225, 30]
-        ]
+        "#F433FF",
+        "#00E11E",
+        "#E100BB",
+        "#00D4E1",
+        "#DD0048",
+        "#F3F315"
+    ]
 
     var size = colors.length
-    var to = colors[Math.floor(Math.random() * size)]
+    var nextColorId = Math.floor(Math.random() * size)
     
-    var txt = document.getElementById('main'),
-        header = document.querySelector('header'),
-        shadow = document.getElementById('shadow'),
-        // for color purpose only
-        squiggly = document.querySelector('h2')
-
-    var centerX = (window.innerWidth * 0.5)
-    var centerY = (txt.clientHeight + header.clientHeight) / 2
-    var frameId = 0
-    
-    // prevent page content clipping logo stuff
-    document.getElementById('block').style.height = 
-        (txt.clientHeight + header.clientHeight) + 'px'
+    var txt = document.getElementById('main')
+    var shadow = document.getElementById('shadow')
+    var squiggly = document.getElementById('dots')
     
     window.onresize = function () {
-        centerX = (window.innerWidth * 0.5)
+        var cx = window.innerWidth * 0.5
+        var cy = window.innerHeight * 0.1
+
+        if (cy - txt.clientHeight * 0.5 - 10 < 0)
+            cy = txt.clientHeight * 0.5 + 10
+
+        txt.style.left =      cx - txt.clientWidth * 0.5 + 'px'
+        shadow.style.left =   cx - shadow.clientWidth * 0.5 - 5 + 'px'
+        squiggly.style.left = cx - squiggly.clientWidth * 0.5 + 'px'
+
+        txt.style.top =      cy - txt.clientHeight * 0.5 + 'px'
+        shadow.style.top =   cy - shadow.clientHeight * 0.5 + 5 + 'px'
+        squiggly.style.top = cy + txt.clientHeight + 'px'
+
+        document.body.style["margin-top"] = 20 + 
+            (cy + txt.clientHeight + squiggly.clientHeight) + 'px'
     }
-    
-    // stop anim. when not in focus (save some battery, man)
-    window.onblur = function () {
-        if (frameId !== 0)
-            cancelAnimationFrame(frameId)
-        frameId = 0
-    }
-    // and enable it when we have focus
-    window.onfocus = function () {
-        if (frameId === 0)
-            frameId = requestAnimationFrame(render)
-    }
+    window.onresize()
 
-    var move = function (t) {
-        // origin point
-        var ox = centerX - 8 * Math.cos(t * 0.0005)
-        var oy = centerY + 8 * Math.sin(t * 0.0005)
+    // sometimes needed 
+    setTimeout(window.onresize, 25)
 
-        // SHAI text pos
-        var x = ox - txt.clientWidth * 0.5
-        var y = oy - txt.clientHeight * 0.5
-
-        // shadow SHAI text pos
-        var sx = ox + 3 * Math.cos(t * 0.001) - 4 - txt.clientWidth * 0.5
-        var sy = oy + 3 * Math.sin(t * 0.001) + 4 - txt.clientHeight * 0.5
-
-        // ~~~~ text pos
-        var hx = ox - header.clientWidth * 0.5
-        var hy = oy
+    var move = function () {
+        var y = Math.random() * 16 - 8
+        var x = Math.random() * 16 - 8
 
         translate(txt, x, y)
-        translate(shadow, sx, sy)
-        translate(header, hx , hy)
+        translate(shadow, 
+            Math.random() * 16 - 8, 
+            Math.random() * 16 - 8)
+        translate(squiggly, x, y)
+
+        setTimeout(move, 4000)
     }
 
     var color = function () {
-        var clr = colors[Math.floor(Math.random() * size)]
+        var id = Math.floor(Math.random() * size)
+        if (id === nextColorId)
+            id = (nextColorId + 1) % size
 
         txt.style.color = 
-        squiggly.style.color = 'rgb(' + 
-            clr[0] + ',' + 
-            clr[1] + ',' + 
-            clr[2] + 
-        ')'
+        squiggly.style.color = colors[id]
 
-        shadow.style.color = 'rgb(' + 
-            to[0] + ',' + 
-            to[1] + ',' + 
-            to[2] + 
-        ')'
+        shadow.style.color = colors[nextColorId]
 
-        to = colors[Math.floor(Math.random() * size)]
-
-        setTimeout(color, Math.floor(Math.random() * 2000) + 3000)
-    }
-
-    var render = function (ts) {
-        move(ts)
-        frameId = requestAnimationFrame(render)
+        nextColorId = Math.floor(Math.random() * size)
+        setTimeout(color, Math.floor(Math.random() * 2000 - 1000) + 4000)
     }
 
     // let's kick it!
     color()
-    frameId = requestAnimationFrame(render)
+    move()
 }
